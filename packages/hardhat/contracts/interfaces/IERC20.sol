019c671dee3a41af9bdc1a9ea442ffa95527f257
Alejandro <alejandrogarce2007@gmail.com>
2026-05-23 13:49:03 +0200

    fix: add transferFrom to IERC20 interface

--- packages/hardhat/contracts/interfaces/IERC20.sol
+++ packages/hardhat/contracts/interfaces/IERC20.sol
@@ -4,4 +4,5 @@ pragma solidity 0.8.35;
 interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
